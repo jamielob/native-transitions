ca24459d7863333529ff09191734f67753c45713
Jamie Loberman <jamie@beautifulcoding.com>
2016-02-07 13:49:33 -0800

    Revert 2da15f8..12a5367
    This rolls back to commit 2da15f8ee0c8ed010bf299c1600d3787e29c3be0.

--- packages/native-transitions-tabs/native-transitions-tabs.js
+++ packages/native-transitions-tabs/native-transitions-tabs.js
@@ -13,33 +13,28 @@ if (Meteor.isCordova) {
 
 if (Meteor.isClient) {
 
-	Session.setDefault('ntCurrentTab', 'nt-tab-1');
-	Session.setDefault('ntHeavyLoaded', 'nt-heavy-tab-1');
+	Session.setDefault('ntCurrentTab', 'nt-tab1');
+	Session.setDefault('ntShowContent', 'nt-show-tab1');
 
 	// Watch tab pages changes
 	Tracker.autorun(function () {
 		//Get the current tab param
-		var tab = FlowRouter.getQueryParam("nt-tab");
+		var tab = FlowRouter.getQueryParam("tab");
 		if (tab) {
-			//Clear the tabs hidden helper (used to know when we should disable scrolling on the tabs because of the z-index problem of overalying scrolling divs)
-			Session.set('ntTabsHidden', '');
 			//Convert the to the class name
-			var tabClass = 'nt-tab-' + tab;
+			var tabClass = 'nt-' + tab;
 			//Check if the the class is any different to the current tab set (if it's not, we're just going back in history and don't need to do anything)
 			if (tabClass !== Session.get('ntCurrentTab')) {
 				//Set the current tabe to the current param
-				Session.set('ntCurrentTab', 'nt-tab-' + tab);
+				Session.set('ntCurrentTab', 'nt-' + tab);
 				//Reset the show content var (so we can show a loading spinner)
-				Session.set('ntHeavyLoaded', '');
+				Session.set('ntShowContent', '');
 				//By deferring this, the inital transition between tabs is instant, no matter how big the fui-scroll dom is
 				Meteor.defer(function() {
 					//Set the show content session to the class name
-					Session.set('ntHeavyLoaded', 'nt-heavy-tab-' + tab);
+					Session.set('ntShowContent', 'nt-show-' + tab);
 				});
 		 	}
- 		} else {
- 			//Tabs are hidden because the param isn't there
- 			Session.set('ntTabsHidden', 'nt-tabs-hidden');
  		}
 	});
 
@@ -48,16 +43,12 @@ if (Meteor.isClient) {
 		nt.transition();
 	});
 
-	Template.registerHelper('ntTabsHidden', function() {
-		return Session.get('ntTabsHidden');
-	});
-
 	Template.registerHelper('ntCurrentTab', function() {
 		return Session.get('ntCurrentTab');
 	});
 
-	Template.registerHelper('ntHeavyLoaded', function() {
-		return Session.get('ntHeavyLoaded');
+	Template.registerHelper('ntShowContent', function() {
+		return Session.get('ntShowContent');
 	});
 
 
