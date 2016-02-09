0a996610b89853eeee9dd210a1bfcbf5657b31bb
Jamie Loberman <jamie@beautifulcoding.com>
2016-02-09 12:18:25 -0800

    Update cordova plugin

--- packages/native-transitions/package.js
+++ packages/native-transitions/package.js
@@ -14,7 +14,7 @@ Cordova.depends({
     // 'com.telerik.plugins.nativepagetransitions': '0.6.0'
     //Fixes topOffsetPixel adding margin https://github.com/Telerik-Verified-Plugins/NativePageTransitions/tarball/7409e2f9a28a069095862d7b1d14d03207acffb9
     //Fixes console log warning 
-    'com.telerik.plugins.nativepagetransitions': 'https://github.com/Telerik-Verified-Plugins/NativePageTransitions/commit/e0b9839315353db9329cbf7fe25fb5f548c1dd7f' 
+    'com.telerik.plugins.nativepagetransitions': 'https://github.com/Telerik-Verified-Plugins/NativePageTransitions/tarball/e0b9839315353db9329cbf7fe25fb5f548c1dd7f' 
 });
 
 Package.onUse(function(api) {
