6d7ac6f58ae3152b33e0f1cd1c7c1e9920b9714f
Jamie Loberman <jamie@beautifulcoding.com>
2016-03-08 15:57:06 -0800

    Update to 0.6.3 cordova plugin

--- packages/native-transitions/package.js
+++ packages/native-transitions/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jamielob:native-transitions',
-  version: '1.1.0',
+  version: '1.2.0',
   // Brief, one-line summary of the package.
   summary: 'Actual native transitions for your Meteor app on iOS and Android',
   // URL to the Git repository containing the source code for this package.
@@ -16,9 +16,11 @@ Cordova.depends({
     //Fixes console log warning 
     //'com.telerik.plugins.nativepagetransitions': 'https://github.com/Telerik-Verified-Plugins/NativePageTransitions/tarball/e0b9839315353db9329cbf7fe25fb5f548c1dd7f' 
     //0.6.1
-    'com.telerik.plugins.nativepagetransitions': 'https://github.com/Telerik-Verified-Plugins/NativePageTransitions/tarball/2d7cbc23bb10312e676f3ef51ca42b8fbe4d4105'
+    //'com.telerik.plugins.nativepagetransitions': 'https://github.com/Telerik-Verified-Plugins/NativePageTransitions/tarball/2d7cbc23bb10312e676f3ef51ca42b8fbe4d4105'
     //0.6.2
     //'com.telerik.plugins.nativepagetransitions': 'https://github.com/Telerik-Verified-Plugins/NativePageTransitions/tarball/860e2f60df294195a1bdb07103f7d954f2bd9af2'
+    //0.6.3
+    'com.telerik.plugins.nativepagetransitions': 'https://github.com/Telerik-Verified-Plugins/NativePageTransitions/tarball/611528b80cb5e50c836da74bdcf5e26793fcc0d6'
 });
 
 Package.onUse(function(api) {
